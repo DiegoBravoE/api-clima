1bed4ee42c136b7c7cde270a5454f2b3e0d40b99
Diego Bravo <dbravo_2015@hotmail.com>
2022-05-06 04:35:39 -0300

    api-clima

---
+++ src/ApiComponente.js
@@ -0,0 +1,59 @@
+import axios, { Axios } from 'axios'
+import { useEffect,useState  } from 'react';
+const ApiComponente = () => {
+
+
+const[weather,setWeather]=useState({})
+const[isCelsius,setIsCelsius]=useState(false)
+
+useEffect(()=>{
+
+function success(pos) {
+    var crd = pos.coords;
+  
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=6b47f1852b39235a024ae0c85ce12472`)
+       .then(res=>{setWeather(res.data)
+                  
+    })
+            
+       
+  }
+  
+  function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    
+    navigator.geolocation.getCurrentPosition(success, error);
+},[])
+console.log(weather)
+const changeTemp = ()=>setIsCelsius(!isCelsius)
+
+
+    return (
+        <div className="App">
+
+            <h1>Weather App</h1>
+            <h3>Country : "{weather.sys?.country}" <br />City : {weather.name}</h3>
+        <div className="icon-temp">
+            <img  src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`} alt="" />
+            <h4>{isCelsius ? `${Math.round(weather.main?.temp) } F°`:`${Math.round((weather.main?.temp -32 *5)/9)} C°`} </h4>
+            
+
+        </div>
+        <div className="date">
+
+            <p><strong>Weather : {weather.weather?.[0].description}</strong></p>
+            <p><strong>Humidity : {weather.main?.humidity}</strong></p>
+            <p><strong>Wind : {weather.wind?.speed} km/h</strong></p>
+        </div>
+           <button onClick={changeTemp}  > <strong>Change F°/C°</strong></button>
+
+        </div>
+    );
+};
+
+export default ApiComponente;
