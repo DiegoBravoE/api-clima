6e4f5bf919ecec0eb281ddc51b9b33c5d920ede4
Diego Bravo <dbravo_2015@hotmail.com>
2022-05-10 01:40:04 -0300

    final

--- src/ApiComponente.js
+++ src/ApiComponente.js
@@ -13,10 +13,6 @@ useEffect(()=>{
 function success(pos) {
     var crd = pos.coords;
   
-    console.log('Your current position is:');
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
     axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=6b47f1852b39235a024ae0c85ce12472`)
        .then(res=>{setWeather(res.data)
         setIsLoading(false)        
