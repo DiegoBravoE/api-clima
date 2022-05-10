ad343ed98b6f525011bfafd3caf3cd55ad8f6237
Diego Bravo <dbravo_2015@hotmail.com>
2022-05-10 01:18:00 -0300

    indexado

--- src/ApiComponente.js
+++ src/ApiComponente.js
@@ -36,43 +36,38 @@ const changeTemp = ()=>setIsCelsius(!isCelsius)
 
 
     return (
-        
-     
+
          <div className="App">
           {
-isLoading?(
-    
-    <div class="loadingio-spinner-eclipse-3av56ck46fl">
-        <div class="ldio-8zcwjji7if5">
-        <div></div>
-        </div></div>
+                isLoading?
+                (
+                                
+                <div class="loadingio-spinner-eclipse-3av56ck46fl">
+                    <div class="ldio-8zcwjji7if5">
+                    <div></div>
+                    </div></div>
 
+                 ):(
+                        <div>
+                            <h1>Weather App</h1>
+                            <h3> City : {weather.name}, '{weather.sys?.country}'</h3>
+                        <div className="icon-temp">
+                            <img  src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`} alt="" />
+                            <h4>{isCelsius ? `${Math.floor(weather.main?.temp -273.15)*9/5+32 } F°`:`${Math.floor((weather.main?.temp -273.15 ))} C°`} </h4>
+                        
+                        </div>
+                        <div className="date">
 
+                            <p><strong>Weather : {weather.weather?.[0].description}</strong></p>
+                            <p><strong>Humidity : {weather.main?.humidity} %</strong></p>
+                            <p><strong>Wind : {weather.wind?.speed} km/h</strong></p>
+                            <p><strong>Pressure : {weather.main?.pressure} </strong></p>
+                        </div>
+                        <button onClick={changeTemp}  > <strong>Change F°/C°</strong></button>
 
-           ):(
-
-            <div>
-
-
-                <h1>Weather App</h1>
-                <h3> City : {weather.name}, '{weather.sys?.country}'</h3>
-            <div className="icon-temp">
-                <img  src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@2x.png`} alt="" />
-                <h4>{isCelsius ? `${Math.floor(weather.main?.temp -273.15)*9/5+32 } F°`:`${Math.floor((weather.main?.temp -273.15 ))} C°`} </h4>
-            
-            </div>
-            <div className="date">
-
-                <p><strong>Weather : {weather.weather?.[0].description}</strong></p>
-                <p><strong>Humidity : {weather.main?.humidity} %</strong></p>
-                <p><strong>Wind : {weather.wind?.speed} km/h</strong></p>
-                <p><strong>Pressure : {weather.main?.pressure} </strong></p>
-            </div>
-            <button onClick={changeTemp}  > <strong>Change F°/C°</strong></button>
-
-            </div>
-    
-               )
+                        </div>
+        
+                    )
             }
             </div>
        
